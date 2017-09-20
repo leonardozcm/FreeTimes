1812efa1789689404bc5fc2f4858b0b511ae4f27
zcm <597714572@qq.com>
2017-09-20 13:12:08 +0800

    90%completed

--- app/src/main/java/com/example/com/freetimes/DayItemTouchHelperCallback.java
+++ app/src/main/java/com/example/com/freetimes/DayItemTouchHelperCallback.java
@@ -1,5 +1,7 @@
 package com.example.com.freetimes;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -35,5 +37,9 @@ public class DayItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         dayseventadapter.onItemDismiss(viewHolder.getAdapterPosition());
+        Context context=viewHolder.itemView.getContext();
+        Intent intent = new Intent(context, LongRunningService.class);
+        intent.putExtra("isRepeat",true);
+        context.startService(intent);
     }
 }
